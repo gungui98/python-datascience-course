b7bbd25454c9c2cb60b5e075bd38b14d2ae66102
khoahd <109609582+khoavpt@users.noreply.github.com>
2023-05-08 17:44:44 +0700

    week5

--- week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
+++ week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
@@ -1,4 +1,53 @@
 import numpy as np
 
+def mean(nplist):
+    '''
+    Return the mean of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.mean(axis=0)), list(nplist.mean(axis=1)), nplist.mean()]
+
+def variance(nplist):
+    '''
+    Return the variance of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.var(axis=0)), list(nplist.var(axis=1)), nplist.var()]
+
+def standard_deviation(nplist):
+    '''
+    Return the standard deviation of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.std(axis=0)), list(nplist.std(axis=1)), nplist.std()]
+
+def max(nplist):
+    '''
+    Return the max of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.max(axis=0)), list(nplist.max(axis=1)), nplist.max()]
+
+def min(nplist):
+    '''
+    Return the min of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.min(axis=0)), list(nplist.min(axis=1)), nplist.min()]
+
+def sum(nplist):
+    '''
+    Return the sum of a list in this format: [axis1, axis2, flattened]
+    '''
+    return [list(nplist.sum(axis=0)), list(nplist.sum(axis=1)), nplist.sum()]
+
+
 def calculate(list):
-    return calculations
+    if (len(list) != 9):
+        raise ValueError("List must contain nine numbers.")
+
+    nplist = np.reshape(np.array(list), newshape=(3,3))
+
+    return {'mean': mean(nplist),
+            'variance': variance(nplist),
+            'standard deviation': standard_deviation(nplist),
+            'max': max(nplist),
+            'min': min(nplist),
+            'sum': sum(nplist)}
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
