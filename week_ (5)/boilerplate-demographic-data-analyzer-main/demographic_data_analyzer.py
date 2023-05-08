383afa3517ced90fa2752101c3255cd8a0d29c29
hueptk0711 <hueptka2k23@cvp.vn>
2023-05-08 17:22:49 +0700

    w5

--- week_ (5)/boilerplate-demographic-data-analyzer-main/demographic_data_analyzer.py
+++ week_ (5)/boilerplate-demographic-data-analyzer-main/demographic_data_analyzer.py
@@ -1,11 +1,9 @@
 import pandas as pd
-import os
 
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    cwd = os.getcwd()
-    df = pd.read_csv(f'{cwd}/adult.data.csv')
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = df['race'].value_counts()
