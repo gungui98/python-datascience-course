7d4c008b915fe980390bb65af38fd5932bb67111
Tohrumi <ducanhpro2508@gmail.com>
2023-05-08 21:49:08 +0700

    submit week 5

--- week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
+++ week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
@@ -1,4 +1,15 @@
 import numpy as np
 
-def calculate(list):
+def calculate(llist):
+    if len(llist) != 9: raise ValueError("List must contain nine numbers.")
+    llist = np.array(llist)
+    mat = llist.reshape(3, 3)
+    calculations = {
+        'mean': [list(np.mean(mat, axis = 0)), list(np.mean(mat, axis = 1)), llist.mean()],
+        'variance': [list(np.var(mat, axis = 0)), list(np.var(mat, axis = 1)), llist.var()],
+        'standard deviation': [list(np.std(mat, axis = 0)), list(np.std(mat, axis = 1)), llist.std()],
+        'max': [list(np.max(mat, axis = 0)), list(np.max(mat, axis = 1)), llist.max()],
+        'min': [list(np.min(mat, axis = 0)), list(np.min(mat, axis = 1)), llist.min()],
+        'sum': [list(np.sum(mat, axis = 0)), list(np.sum(mat, axis = 1)), llist.sum()]
+    }
     return calculations
