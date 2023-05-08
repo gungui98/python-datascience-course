00a61af5c293155b223bfd7b7970d22ecc2e7df8
Nguyễn Việt Bắc <127400120+bacngv@users.noreply.github.com>
2023-05-08 17:36:07 +0700

    week5

--- week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
+++ week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
@@ -1,48 +1,25 @@
 import numpy as np
 
-import numpy as np
-
 def calculate(list):
-    if ( len(list) < 9 ):
-        raise ValueError("List must contain nine numbers.")
-    else: 
+    if len(list)!=9:
+      raise ValueError('List must contain nine numbers.')
+    else:
+      matrix = np.array(list).reshape(3,3)
+    
+    meanl = [(matrix.mean(axis=0).tolist()), (matrix.mean(axis=1).tolist()), (matrix.flatten().mean())]
+    varl = [(matrix.var(axis=0).tolist()), (matrix.var(axis=1).tolist()), (matrix.flatten().var())]
+    stdl = [(matrix.std(axis=0).tolist()), (matrix.std(axis=1).tolist()), (matrix.flatten().std())]
+    maximum = [(matrix.max(axis=0).tolist()), (matrix.max(axis=1).tolist()), (matrix.flatten().max())]
+    minimum = [(matrix.min(axis=0).tolist()), (matrix.min(axis=1).tolist()), (matrix.flatten().min())]
+    suml = [(matrix.sum(axis=0).tolist()), (matrix.sum(axis=1).tolist()), (matrix.flatten().sum())]
 
-        m = np.array(list).reshape(3,3)
-        f = m.flatten()
-
-        mean_axis0 = np.mean(m, axis = 0, dtype = float).tolist()
-        mean_axis1 = np.mean(m, axis = 1, dtype = float).tolist()
-        mean_axis_flat = np.mean(f).tolist()
-
-        var_axis0 = np.var(m, axis = 0, dtype = float).tolist()
-        var_axis1 = np.var(m, axis = 1, dtype = float).tolist()
-        var_axis_flat = np.var(f, dtype = float).tolist()
-
-        sd_axis0 = np.std(m, axis = 0, dtype = float).tolist()
-        sd_axis1 = np.std(m, axis = 1, dtype = float).tolist()
-        sd_axis_flat = np.std(f, dtype = float).tolist()
-
-        max_axis0 = np.max(m, axis = 0).tolist()
-        max_axis1 = np.max(m, axis = 1).tolist()
-        max_axis_flat = np.max(f).tolist()
-
-        min_axis0 = np.min(m, axis = 0).tolist()
-        min_axis1 = np.min(m, axis = 1).tolist()
-        min_axis_flat = np.min(f).tolist()
-
-        sum_axis0 = np.sum(m, axis = 1, dtype = float).tolist()
-        sum_axis1 = np.sum(m, axis = 0, dtype = float).tolist()
-        sum_axis_flat = np.sum(f, dtype = float).tolist()
-
-        calculations = {
-            'mean': [mean_axis0,mean_axis1,mean_axis_flat],
-            'variance': [var_axis0,var_axis1,var_axis_flat],
-            'standard deviation': [sd_axis0,sd_axis1,sd_axis_flat],
-            'max': [max_axis0,max_axis1,max_axis_flat],
-            'min': [min_axis0,min_axis1,min_axis_flat],
-            'sum': [sum_axis0,sum_axis1,sum_axis_flat]
-         }
-
-
+    calculations = {
+      "mean": meanl,
+      "variance": varl,
+      "standard deviation": stdl,
+      "max": maximum,
+      "min": minimum,
+      "sum": suml,
+    }
 
     return calculations
