d281f8b3ac4d00a0c346877b5628474f7ddf88ae
ineedsumsnack <vuminhtien2004@gmail.com>
2023-05-15 16:51:53 +0700

    add 7

--- week_ (7)/boilerplate-page-view-time-series-visualizer-main/time_series_visualizer.py
+++ week_ (7)/boilerplate-page-view-time-series-visualizer-main/time_series_visualizer.py
@@ -5,17 +5,19 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv',
+                index_col= ['date'],
+                 parse_dates=['date'])
+
 
 # Clean data
-df = None
-
+df = df.loc[(df["value"] >= df["value"].quantile(0.025)) & (df["value"] <= df["value"].quantile(0.975))
+]
 
 def draw_line_plot():
     # Draw line plot
-
-
-
+    fig, ax = plt.subplots(figsize= (10,5))
+    sns.lineplot(data=df, c = 'r')
 
 
     # Save image and return fig (don't change this part)
@@ -24,10 +26,19 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month_name()
+    df_bar['month_number'] = df_bar.index.month
+
+    df_bar = df_bar.groupby(['year','month']).mean().reset_index().sort_values(['month_number'])
+
+
+
 
     # Draw bar plot
-
+    fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100)
+    sns.barplot(df_bar, x= 'year', hue='month', y='value', palette='tab10')
 
 
 
@@ -37,15 +48,22 @@ def draw_bar_plot():
     return fig
 
 def draw_box_plot():
+    from time import strptime
+
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
+    df_box['month_num'] = [strptime(x,'%b').tm_mon for x in df_box['month']]
+    df_box = df_box.sort_values(['month_num'])
 
     # Draw box plots (using Seaborn)
+    fig, axes = plt.subplots(1, 2, figsize=(32, 10))
+    
+    sns.boxplot(df_box, x='month', y='value', ax = axes[1])
 
-
+    sns.boxplot(df_box, x='year', y='value', ax = axes[0])
 
 
 
