0e38708ba45ea76e93faf469735c0906d86085b2
Your Name <you@example.com>
2023-05-19 14:13:02 +0700

    week 8 part 1

--- week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
+++ week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
@@ -4,19 +4,23 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
-
+    plt.scatter(x=df["Year"], y=df["CSIRO Adjusted Sea Level"])
 
     # Create first line of best fit
-
+    line = linregress(x=df["Year"], y=df["CSIRO Adjusted Sea Level"], alternative='two-sided')
+    plt.axline((df["Year"].min(), df["Year"].min() * line.slope + line.intercept), (2050, 2050 * line.slope + line.intercept), color="red")
 
     # Create second line of best fit
+    line_2000 = linregress(x=df.loc[df["Year"] >= 2000, "Year"], y=df.loc[df["Year"] >= 2000, "CSIRO Adjusted Sea Level"], alternative='two-sided')
+    plt.axline((2000, 2000 * line_2000.slope + line_2000.intercept), (2050, 2050 * line_2000.slope + line_2000.intercept), color="red")
 
 
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
