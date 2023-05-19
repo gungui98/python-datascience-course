2581f7429c2c1ee4aa14e4fba19dbc537b93711e
Binhdn04 <118729850+Binhdn04@users.noreply.github.com>
2023-05-19 13:40:39 +0700

    Update sea_level_predictor.py

--- week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
+++ week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
@@ -4,7 +4,7 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-    df = pd.read_csv("epa-sea-level.csv")
+    df = pd.read_csv("https://raw.githubusercontent.com/fuzzyray/sea-level-predictor/main/epa-sea-level.csv")
 
     # Create scatter plot
     fig, ax = plt.subplots(figsize=(16, 9))
