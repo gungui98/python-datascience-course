8d2e0123c12415a266613b13670e6674b877d3f6
Khaidv <111173070+khaidv123@users.noreply.github.com>
2023-05-21 15:48:55 +0700

    Add files via upload

--- week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
+++ week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
@@ -5,49 +5,29 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+
     df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-    df.plot(kind = 'scatter', x = 'Year', y = 'CSIRO Adjusted Sea Level')  
-    
+
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+
     # Create first line of best fit
-    #get the slope and y intercept 
-    slope, y_int, r_val, p_val, std_err = linregress(x = df['Year'], y = df['CSIRO Adjusted Sea Level'])
-    #create the extra years
-    extra_years = np.arange(2014, 2050)
-    #convert the array to a Series
-    extra_years = pd.Series(extra_years)
-    #get the x value
-    x = df['Year']
-    #get the x for the linear plot
-    futX= x.append(extra_years, ignore_index= True)
-    
-    #plot the linear plot of futX and y given mx + b
-    plt.plot(futX, slope*futX + y_int)
-    #set the labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches')
-    plt.title('Rise in Sea Level')
+
+    lr_1880_2012 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    plt.plot(range(1880, 2051, 1), lr_1880_2012.slope * range(1880, 2051, 1) + lr_1880_2012.intercept)
 
     # Create second line of best fit
-    #create a new df that is only the data from 2000 onwards
-    modern = df.loc[df['Year'] >= 2000]
-    #get the new slop and y intercept
-    modSlope, mody_int, r_val, p_val, std_err = linregress(x = modern['Year'], y = modern ['CSIRO Adjusted Sea Level'])
 
-    #get the x for the second scatter plot
-    x2 = modern['Year']
-    #get the futX based on the additional years needed
-    futX2 = x2.append(extra_years,ignore_index= True)
-
-    #plot the line of best fit
-    plt.plot(futX2, modSlope * futX2 + mody_int)
+    lr_2000_2012 = linregress(df.query('Year >= 2000')['Year'], 
+                              df.query('Year >= 2000')['CSIRO Adjusted Sea Level'])
+    plt.plot(range(2000, 2051, 1), lr_2000_2012.slope * range(2000, 2051, 1) + lr_2000_2012.intercept)
 
     # Add labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
+
     plt.title('Rise in Sea Level')
-    
+    plt.ylabel('Sea Level (inches)')
+    plt.xlabel('Year')
+
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
-    return plt.gca()
