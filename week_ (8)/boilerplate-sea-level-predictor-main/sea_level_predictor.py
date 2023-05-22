fb5024865248017c4d73d516466f87434e8d7349
Hà Như Ý <120201377+nhuyha@users.noreply.github.com>
2023-05-22 17:15:19 +0700

    update

--- week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
+++ week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
@@ -1,30 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
 import numpy as np
+from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
-
+    df.plot(kind = 'scatter', x = 'Year', y = 'CSIRO Adjusted Sea Level')  
+    
     # Create first line of best fit
-    lineA = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    xA = np.arange(df['Year'].min(),2050,1)
-    yA = xA*lineA.slope + lineA.intercept
-
-    plt.plot(xA,yA)
+    #get the slope and y intercept 
+    slope, y_int, r_val, p_val, std_err = linregress(x = df['Year'], y = df['CSIRO Adjusted Sea Level'])
+    #create the extra years
+    extra_years = np.arange(2014, 2050)
+    #convert the array to a Series
+    extra_years = pd.Series(extra_years)
+    #get the x value
+    x = df['Year']
+    #get the x for the linear plot
+    futX = x.append(extra_years)
+    
+    #plot the linear plot of futX and y given mx + b
+    plt.plot(futX, slope*futX + y_int)
+    #set the labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches')
+    plt.title('Rise in Sea Level')
 
     # Create second line of best fit
-    df_2000 = df[df['Year'] >= 2000]
+    #create a new df that is only the data from 2000 onwards
+    modern = df.loc[df['Year'] >= 2000]
+    #get the new slop and y intercept
+    modSlope, mody_int, r_val, p_val, std_err = linregress(x = modern['Year'], y = modern ['CSIRO Adjusted Sea Level'])
 
-    lineB = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    xB = np.arange(2000,2050,1)
-    yB = xB*lineB.slope + lineB.intercept
+    #get the x for the second scatter plot
+    x2 = modern['Year']
+    #get the futX based on the additional years needed
+    futX2 = x2.append(extra_years)
 
-    plt.plot(xB,yB)
+    #plot the line of best fit
+    plt.plot(futX2, modSlope * futX2 + mody_int)
 
     # Add labels and title
     plt.xlabel('Year')
