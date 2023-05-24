24eede770f1708c8d4dc7a59d9eed9156a9b1c4a
Nhat Do <dominhnhat2004@gmail.com>
2023-05-24 10:33:12 +0700

    again

--- week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
+++ week_ (8)/boilerplate-sea-level-predictor-main/sea_level_predictor.py
@@ -1,36 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
-import numpy as np
 
 def draw_plot():
     # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
-
+    df = pd.read_csv('/work/epa-sea-level.csv')
+    x = df['CSIRO Adjusted Sea Level']
+    y = df["Year"]
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+    fig, ax = plt.subplots()
+    plt.scatter(x, y)
 
     # Create first line of best fit
-    lineA = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    xA = np.arange(df['Year'].min(),2050,1)
-    yA = xA*lineA.slope + lineA.intercept
-
-    plt.plot(xA,yA)
-
+    res = linregress(x, y)
+    x_prediction = pd.Series([i for i in range (1880, 2051)])
+    y_prediction = res.slope*x_prediction + res.intercept
+    plt.plot(x_prediction, y_prediction, "r")
     # Create second line of best fit
-    df_2000 = df[df['Year'] >= 2000]
-
-    lineB = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    xB = np.arange(2000,2050,1)
-    yB = xB*lineB.slope + lineB.intercept
-
-    plt.plot(xB,yB)
-
+    new_df = df.loc[df['Year'] >= 2000]
+    new_x = new_df['Year']
+    new_y = new_df["CSIRO Adjusted Sea Level"]
+    n_res = linregress(new_x, new_y)
+    x_pred2 = pd.Series([i for i in range (2000,2051)])
+    y_pred2 = n_res.slope*x_pred2 + n_res*intercept
+    plt.plot(x_pred2, y_pred2, 'green')
     # Add labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
-    
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.set_title('Rise in Sea Level')
+
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
